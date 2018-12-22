725e9e891a4e5fb232b796ecd77837e5de10fb66
Jamie Skinner <skinner520@gmail.com>
2018-12-22 11:15:21 -0600

    add ebingore, remove proxy from package.json

--- scripts/travisPrepForDeployment.sh
+++ scripts/travisPrepForDeployment.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 ## RUN FROM ROOT
 echo '********* Running Prep Script *********'
-sed -i 's/.static("public")/.static("build")/' ./server/server.js
-
-## update package.json
-sed -i '/proxy/d' ./package.json
 
 echo '********* Finishing Prep... *********'
-
